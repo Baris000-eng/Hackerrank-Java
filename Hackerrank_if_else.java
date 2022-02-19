efc28eba8115920c30d7740902bc669a1e4131b1
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-02-20 00:48:58 +0300

    Update Hackerrank_if_else.java

--- Hackerrank_if_else.java
+++ Hackerrank_if_else.java
@@ -8,7 +8,44 @@ import java.util.regex.*;
 
 public class Solution {
 
+/*Task
+Given an integer, , perform the following conditional actions:
 
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+Explanation
+
+Sample Case 0: 
+ is odd and odd numbers are weird, so we print Weird.
+
+Sample Case 1: 
+ and  is even, so it isn't weird. Thus, we print Not Weird.*/
 
     private static final Scanner scanner = new Scanner(System.in);
 
