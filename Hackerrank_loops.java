94dc1217aafd8715ad327dbb54d35fa2ffc7a771
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-02-20 02:01:57 +0300

    Update Hackerrank_loops.java

--- Hackerrank_loops.java
+++ Hackerrank_loops.java
@@ -13,6 +13,38 @@ import static java.util.stream.Collectors.toList;
 
 
 public class Solution {
+   /* In this challenge, we're going to use loops to help us do some simple math.
+
+Task
+Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print  lines of output; each line  (where ) contains the  of  in the form:
+N x i = result.
+
+Sample Input
+
+2
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+*/
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
