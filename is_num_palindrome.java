b55ab83d618ddb0392b782c8baef901fa7ca7188
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2022-02-20 22:15:34 +0000

    My 2nd and 3rd implementations to check whether a number is palindrome or not

--- is_num_palindrome.java
+++ is_num_palindrome.java
@@ -19,4 +19,34 @@ class Solution {
          
         
 }  
+
+//2nd implementation to check whether a number is palindrome or not 
+ public boolean isPalindromeVersion2(int x) {
+       String str= Integer.toString(x);//convert to a string
+       String s="";
+       for(int i=str.length()-1;i>=0;i--){
+           s+=str.charAt(i);
+       }
+       if(s.equals(str)){
+           return true;
+       } else {
+           return false;
+       }
+        
+}  
+
+//3rd way to check whether a number is palindrome
+public boolean isPalindrome(int x) {
+       boolean isPalindrome=false;
+       String str= Integer.toString(x);
+       for(int i=str.length()-1;i>=0;i--){
+           for(int j= 0; j<str.length();j++){
+               if(str.charAt(i)==str.charAt(j)){
+                  isPalindrome=true;
+               }
+           }
+       }
+       return isPalindrome; 
+}
+
 }  
