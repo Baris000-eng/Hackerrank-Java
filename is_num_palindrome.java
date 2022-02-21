bf74564b13f8168c2ee427c495549d7c48acadee
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-02-21 18:32:51 +0300

    Update is_num_palindrome.java

--- is_num_palindrome.java
+++ is_num_palindrome.java
@@ -62,3 +62,26 @@ public boolean isPalindrome(int x) {
 }
 
 }  
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String blank="";
+       for(int i=A.length()-1;i>=0;i--){
+           blank+=A.charAt(i);
+       }
+       if(blank.equals(A)){
+           System.out.println("Yes");
+       } else {
+           System.out.println("No");
+       }
+    }
+}
+
