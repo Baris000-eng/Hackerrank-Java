57eea9a95f37e44116a0537914a5d4e23dbab4eb
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2022-03-26 09:32:40 +0000

    my power function

---
+++ myPowerFunc.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0 && x!=0){
+            return 1;
+        }
+        
+        
+        if(n<0 && x!=0){
+            x=1/x;
+            n=-n;
+        }
+        
+        double mult=1.0;
+        for(int i=0;i<n;i++){
+            mult=mult*x;
+        }
+        
+        return mult;
+    }
+}
