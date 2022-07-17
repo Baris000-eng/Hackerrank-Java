6a07e220eaa5c0be95931333536616e8773df1ef
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2022-07-17 21:30:44 +0000

    matrix subtraction

---
+++ matrixSubtraction.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+    //constructing 2 matrices
+    int [][] matrix1={
+                   {1,2,3},{2,3,4},{1,1,1}
+               };    
+    int [][] matrix2={
+                   {4,5,6},{7,8,9},{4,5,0}
+               };    
+
+
+    int i = 0;
+    int j = 0;
+    
+    for(i=0;i<3;i++){    
+        for(j=0;j<3;j++){    
+            System.out.println(matrix1[i][j]-matrix2[i][j]+" ");   //summing the corresponding elements of the matrices and printing each of the sum in a seperate line
+        }    
+    }    
+}
+}
