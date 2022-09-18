6a2a7a60533e1d81f34ef95e0da5d5e3d165f37b
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2022-09-18 10:06:56 +0300

    Add files via upload

---
+++ JavaBigInteger.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        BigInteger b1 = scan.nextBigInteger();
+        BigInteger b2 = scan.nextBigInteger();
+        System.out.println(b1.add(b2));
+        System.out.println(b1.multiply(b2));
+    }
+}
+
+/* In this problem, you have to add and multiply huge numbers! These numbers are so big that you can't contain them in any ordinary data types like a long integer.
+
+Use the power of Java's BigInteger class and solve this problem.
+
+Input Format
+
+There will be two lines containing two numbers,  and .
+
+Constraints
+
+ and  are non-negative integers and can have maximum  digits.
+
+Output Format
+
+Output two lines. The first line should contain , and the second line should contain . Don't print any leading zeros. */
